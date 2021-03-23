12a26b2442cf237ab9e3d62589116f80a117b5b3
abou <zoobi.92@gmail.com>
2021-03-23 13:06:12 +0530

    basic page is ready

--- routes/login_signup_routes.js
+++ routes/login_signup_routes.js
@@ -18,43 +18,43 @@ router.post('/users', (req, res, next) => {
         }
 
         else {
-            res.status(403).json({msg: 'User Already Exists',users, status : 'signUpFailure'});
+            res.status(403).json({ msg: 'User Already Exists', users, status: 'signUpFailure' });
         }
     })
 })
 
-router.get('/login', (req,  res , next) =>{
+router.get('/login', (req, res, next) => {
 
     var phone_number_in_param = req.param('phone_number');
-    var password_in_param = req.param('password');  
+    var password_in_param = req.param('password');
 
-    UserDetails.findOne({phone_number: phone_number_in_param, password: password_in_param},function(erer, users){
-        if(users == "" || users == null){
-            res.status(403).json({msg: 'Check your ID and Password',users, status : 'loginFailure'});
+    UserDetails.findOne({ phone_number: phone_number_in_param, password: password_in_param }, function (erer, users) {
+        if (users == "" || users == null) {
+            res.status(403).json({ msg: 'Check your ID and Password', users, status: 'loginFailure' });
         }
-        else{
-            res.json({msg: 'Successfully logged in',users, status : 'success'});
+        else {
+            res.json({ msg: 'Successfully logged in', users, status: 'success' });
         }
-        
+
     })
 });
 
 router.get('/users', (req, res, next) => {
     var phone_number = req.param('phone_number');
 
-    UserDetails.find({phone_number : phone_number},function(erer, users){
-        if(erer){
-            res.status(403).json({msg: 'Error in fetching user data', user : users , status : 'failure'});
+    UserDetails.find({ phone_number: phone_number }, function (erer, users) {
+        if (erer) {
+            res.status(403).json({ msg: 'Error in fetching user data', user: users, status: 'failure' });
         }
-        else{
-            res.json({msg : 'Successfully fecthed all users', user : users, status : 'success'});
+        else {
+            res.json({ msg: 'Successfully fetched all users', user: users, status: 'success' });
         }
     })
 })
 
 router.get('/all_users', (req, res, next) => {
-    UserDetails.find(function(erer, users){
-        res.json({msg : 'Successfully fecthed all users', user : users, status : 'success'});
+    UserDetails.find(function (erer, users) {
+        res.json({ msg: 'Successfully fecthed all users', user: users, status: 'success' });
     })
 })
 
